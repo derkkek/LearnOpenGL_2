4690c2c2ce6fdb1b0b5feb65882910cc4b60da6b
Halil Eren Öymez <erenhalil2010@hotmail.com>
2025-04-14 15:42:57 +0300

    mirror implementation with post process on the mirror

--- resource/shaders/framebuffers_screen.f
+++ resource/shaders/framebuffers_screen.f
@@ -3,7 +3,7 @@ out vec4 FragColor;
 in vec2 TexCoords;
 uniform sampler2D screenTexture;
 
-const float offset = 1.0 / 300.0;
+const float offset = 1.0 / 1000.0;
 
 void main()
 {
@@ -20,9 +20,9 @@ void main()
 	);
 	
 	float kernel[9] = float[](
-		1.0 / 16, 2.0 / 16, 1.0 / 16,
-		2.0 / 16, 4.0 / 16, 2.0 / 16,
-		1.0 / 16, 2.0 / 16, 1.0 / 16
+		1, 1, 1,
+		1, -8, 1,
+		1, 1, 1
 	);
 
 	vec3 sampleTex[9];
